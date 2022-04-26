0350a57882d36779f02acc0af14607e5ee73ce9d
kimsongsub <busanbak2000@naver.com>
2022-04-26 22:27:45 +0900

    header backgroundcolor

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 "use strict";
 const MAX_PAGE_NUMBER = 2999;
 
+const headerBackground = document.querySelector(".header-background");
+const section1 = document.querySelector(".section1");
+const section2 = document.querySelector(".section2");
+const section3 = document.querySelector(".section3");
+const section4 = document.querySelector(".section4");
 const section3_1 = document.getElementsByClassName("section3-1")[0];
 const section3_2 = document.getElementsByClassName("section3-2")[0];
 const section3_3 = document.getElementsByClassName("section3-3")[0];
@@ -8,20 +13,26 @@ const section3_3 = document.getElementsByClassName("section3-3")[0];
 const rigthArrow = document.querySelector("#right_arrow");
 const leftArrow = document.querySelector("#left_arrow");
 
-const section3 = [section3_1, section3_2, section3_3];
+const section3Array = [section3_1, section3_2, section3_3];
 
 let i = 0;
 
 function rightSlide() {
-  section3[i % 3].classList.add("hide-element");
-  section3[(i + 1) % 3].classList.remove("hide-element");
+  section3Array[i % 3].classList.add("hide-element");
+  section3Array[(i + 1) % 3].classList.remove("hide-element");
+  const section3Background = section3Array[(i + 1) % 3].querySelector("div");
+  section3.style.backgroundColor =
+    getComputedStyle(section3Background).backgroundColor;
 
   i++;
 }
 
 function leftSlide() {
-  section3[i % 3].classList.add("hide-element");
-  section3[(i + 2) % 3].classList.remove("hide-element");
+  section3Array[i % 3].classList.add("hide-element");
+  section3Array[(i + 2) % 3].classList.remove("hide-element");
+  section3.style.backgroundColor = getComputedStyle(
+    section3Array[(i + 1) % 3]
+  ).backgroundColor;
 
   if (i == 0) {
     i = MAX_PAGE_NUMBER;
@@ -32,3 +43,26 @@ function leftSlide() {
 
 rigthArrow.addEventListener("click", rightSlide);
 leftArrow.addEventListener("click", leftSlide);
+
+function findScrollInSectionNumber() {
+  if (window.scrollY < section1.clientHeight) {
+    return section1;
+  } else if (window.scrollY < section1.clientHeight + section2.clientHeight) {
+    return section2;
+  } else if (
+    window.scrollY <
+    section1.clientHeight + section2.clientHeight + section3.clientHeight
+  ) {
+    return section3;
+  } else {
+    return section4;
+  }
+}
+
+function handleScroll() {
+  headerBackground.style.backgroundColor = getComputedStyle(
+    findScrollInSectionNumber()
+  ).backgroundColor;
+}
+
+document.addEventListener("scroll", handleScroll);
