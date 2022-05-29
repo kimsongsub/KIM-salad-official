8ac8c846cc5c22e83fbc2136c735fa26176e2121
kimsongsub <busanbak2000@naver.com>
2022-05-30 02:49:32 +0900

    Update food-ingredients.js

--- js/food-ingredients.js
+++ js/food-ingredients.js
@@ -43,13 +43,19 @@ function handleOtherFruits(fruit) {
   fruit.dataset.color = color;
   window.scroll({
     behavior: "smooth",
-    top: 140,
+    top: 0,
   });
 }
 
 function handleFruitsIngredientsJsonData(fruit, jsonData) {
   const mainEffects = document.querySelectorAll(".main-fruit-effect");
   const mainIngredients = document.querySelectorAll(".main-fruit-ingredient");
+  const fruitIngredientType = document.querySelectorAll(
+    ".fruit-ingredient-type"
+  );
+  const fruitIngredientAmount = document.querySelectorAll(
+    ".fruit-ingredient-amount"
+  );
   for (let i = 0; i < 3; i++) {
     mainEffects[i].innerText = Object.keys(jsonData.fruits.effects[fruit.alt])[
       i
@@ -57,6 +63,19 @@ function handleFruitsIngredientsJsonData(fruit, jsonData) {
     mainIngredients[i].innerText = Object.values(
       jsonData.fruits.effects[fruit.alt]
     )[i];
+  }
+  for (let i = 0; i < 10; i++) {
+    if (Object.keys(jsonData.fruits.ingredients[fruit.alt])[i] === undefined) {
+      fruitIngredientType[i].innerText = "-";
+      fruitIngredientAmount[i].innerText = "-";
+    } else {
+      fruitIngredientType[i].innerText = Object.keys(
+        jsonData.fruits.ingredients[fruit.alt]
+      )[i];
+      fruitIngredientAmount[i].innerText = Object.values(
+        jsonData.fruits.ingredients[fruit.alt]
+      )[i];
+    }
   }
 }
 
@@ -79,7 +98,7 @@ function handleOtherVegitables(vegitable) {
   vegitable.dataset.color = color;
   window.scroll({
     behavior: "smooth",
-    top: 1590,
+    top: 1450,
   });
 }
 
@@ -88,6 +107,12 @@ function handleVegitablesIngredientsJsonData(vegitable, jsonData) {
   const mainIngredients = document.querySelectorAll(
     ".main-vegitable-ingredient"
   );
+  const vegitableIngredientType = document.querySelectorAll(
+    ".vegitable-ingredient-type"
+  );
+  const vegitableIngredientAmount = document.querySelectorAll(
+    ".vegitable-ingredient-amount"
+  );
   for (let i = 0; i < 3; i++) {
     mainEffects[i].innerText = Object.keys(
       jsonData.vegitables.effects[vegitable.alt]
@@ -95,5 +120,21 @@ function handleVegitablesIngredientsJsonData(vegitable, jsonData) {
     mainIngredients[i].innerText = Object.values(
       jsonData.vegitables.effects[vegitable.alt]
     )[i];
+  }
+  for (let i = 0; i < 10; i++) {
+    if (
+      Object.keys(jsonData.vegitables.ingredients[vegitable.alt])[i] ===
+      undefined
+    ) {
+      vegitableIngredientType[i].innerText = "-";
+      vegitableIngredientAmount[i].innerText = "-";
+    } else {
+      vegitableIngredientType[i].innerText = Object.keys(
+        jsonData.vegitables.ingredients[vegitable.alt]
+      )[i];
+      vegitableIngredientAmount[i].innerText = Object.values(
+        jsonData.vegitables.ingredients[vegitable.alt]
+      )[i];
+    }
   }
 }
