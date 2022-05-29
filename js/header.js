05c35c4bace38a3c22ab0c001373f9ddf2ebf662
kimsongsub <busanbak2000@naver.com>
2022-05-30 02:48:35 +0900

    Update header.js
    add scroll event

--- js/header.js
+++ js/header.js
@@ -1,13 +1,29 @@
 (function () {
   const detailMenuItem = document.querySelectorAll(".inner-detail-page a");
   const headerMenuItem = document.querySelectorAll(".header-menu li");
+  const headerLogo = document.querySelector("#KIM-salad");
+  const headerFruitLink = document.querySelector("#fruits-ingredients");
+  const headerVegiLink = document.querySelector("#vegitables-ingredients");
 
   detailMenuItem.forEach((menuItem) => {
+    menuItem.addEventListener("click", route);
     menuItem.addEventListener("click", handleDetailMenu);
   });
 
   headerMenuItem.forEach((item) => {
     item.addEventListener("click", handleDetailMenu);
+  });
+
+  headerLogo.addEventListener("click", function () {
+    window.scrollTo(0, 0);
+  });
+
+  headerFruitLink.addEventListener("click", function () {
+    window.scrollTo(0, 0);
+  });
+
+  headerVegiLink.addEventListener("click", function () {
+    window.scrollTo(0, 1450);
   });
 })();
 
